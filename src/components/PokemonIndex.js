a65eb7c4945be6910a4b6aee70635c34a513737a
Ivan Szebenszki <szib77@gmail.com>
2019-06-11 14:43:09 +0100

    Search by ability

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,37 +1,55 @@
 import React from 'react'
 import PokemonCollection from './PokemonCollection'
 import PokemonForm from './PokemonForm'
-import { Search } from 'semantic-ui-react'
+import { Search, Grid } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
 
   state = {
     pokemons: [],
+    abilities: [],
     searchTerm: '',
+    abititiesSearchTerm: '',
   }
 
   componentDidMount() {
     fetch('http://localhost:3000/pokemon')
       .then(resp => resp.json())
-      .then(pokemons => this.setState({pokemons}))
+      .then(pokemons => { 
+        this.setState({
+          pokemons,
+          abilities: _.uniq(_.flatten(pokemons.map(pokemon => pokemon.abilities)))
+        })
+      }) 
   }
 
-  handleSeachChange = (e, { value }) => {
+  handleNameSearchChange = (e, { value }) => {
     this.setState({
       searchTerm: value.toLowerCase(),
     })
   }
 
+  handleAbilitySearchChange = (e, { value }) => {
+    this.setState({
+      abititiesSearchTerm: value.toLowerCase(),
+    })
+  }
+
   addPokemon = (pokemon) => {
-    console.log('pokemon', pokemon)
     this.setState({
       pokemons: [...this.state.pokemons, pokemon]
     })
   }
 
+  hasAbility = (pokemon, searchedAbility) => {
+    return pokemon.abilities.find(ability => ability.includes(searchedAbility))
+  }
+
   getFilterPokemons = () => {
-    return this.state.pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(this.state.searchTerm))
+    return this.state.pokemons
+      .filter(pokemon => pokemon.name.toLowerCase().includes(this.state.searchTerm))
+      .filter(pokemon => this.hasAbility(pokemon,this.state.abititiesSearchTerm))
   }
 
   render() {
@@ -42,9 +60,17 @@ class PokemonPage extends React.Component {
           addPokemon={this.addPokemon}
         />
         <br />
-        <Search 
-          onSearchChange={_.debounce(this.handleSeachChange, 500)} 
-          showNoResults={false} />
+        <Grid centered columns={2}>
+          <Search 
+            onSearchChange={_.debounce(this.handleNameSearchChange, 500)} 
+            placeholder="Name..."
+            showNoResults={false} />
+          <Search 
+            onSearchChange={_.debounce(this.handleAbilitySearchChange, 500)} 
+            placeholder="Ability..."
+            showNoResults={false} />
+        </Grid>
+        
         <br />
         <PokemonCollection pokemons={this.getFilterPokemons()} />
         <br />
